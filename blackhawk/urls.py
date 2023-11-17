89a698c7932673f02fd6ecca18a30cf63cd52bb2
Balint Szabo <bsdrip@protonmail.com>
2023-11-17 03:06:59 +0100

    refactor: redirect to scanner from root path

--- blackhawk/urls.py
+++ blackhawk/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("scanner/", include("VulnerabilityScanner.urls")),
+    path("", RedirectView.as_view(url="scanner/", permanent=True)),
 ]
